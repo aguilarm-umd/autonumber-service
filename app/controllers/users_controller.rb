6f3e95866fb57c25738326cc9d30f2cf3dc9c94d
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2023-08-17 11:45:56 -0400

    Use HTMX to delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,8 +55,9 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
+
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.', status: :see_other }
       format.json { head :no_content }
     end
   end
